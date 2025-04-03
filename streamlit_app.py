09eaf55c970cd72aa652cf0a7d073d8538730a54
sumedhx <sumedhsuralkar@gmail.com>
2025-04-03 15:40:39 +0530

    Ui change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import sklearn
 import pickle
 
 model = pickle.load(open("model.pkl", "rb"))
@@ -8,15 +7,20 @@ st.title("IceCream Revenue")
 st.markdown(
     "The dataset contains modifications with regards to the original for illustrative & learning purposes"
 )
+st.markdown(
+    "Model Used - Linear Regression"
+)
+st.write(f"R²: {0.9771}")
+st.write(f"Intercept: {46.18}")
+st.write(f"Coefficient: {21.38}")
 
-temperature = st.slider('Define Temperature?', 0, 50, 20)
-#accommodates = st.slider('How many people does the listing accommodate?', 1, 16, 2)
-#instant_bookable = st.radio(
-    #"Is the listing instantly bookable?",
-    #("True", "False"))
-#instant_bookable = 1 if instant_bookable == "True" else 0
+temperature = st.number_input("Define Temperature?", min_value=0.0, max_value=50.0, value=20.0, step=0.1)
 
-user_input = [[temperature]]#, accommodates, instant_bookable]]
+
+user_input = [[temperature]]
 
 if st.button('Predict?'):
-    st.write("The model predicts that the average tip for this listing is:", model.predict(user_input).round(2))
+    st.write(f"Based on the temperature {temperature} °C, the revenue will be around: ", model.predict(user_input).round(2))
+
+
+# dataset = 'https://github.com/YBIFoundation/Dataset/raw/main/Ice%20Cream.csv'
